ff3c20e31b28c3673a40fd3cd96ba50e7e6233b3
Liza Volyanskaya <you@example.com>
2025-06-26 08:21:20 +0300

    Commit 8

--- js/my.js
+++ js/my.js
@@ -1,4 +1,7 @@
 const button = document.querySelector('.title1')
+const button2 = document.querySelector('.title2')
+const button3 = document.querySelector('.title3')
+const button4 = document.querySelector('.title4')
 let div = document.createElement('div')
 
 div.className= 'alert'
@@ -20,9 +23,70 @@ button.addEventListener('click',  createBox)
 
 function createBox(){
     document.body.append(div)
-    div.innerHTML = `<li>Pieczywo 3 szt</li>`
-     `<li>krzydelka 5 szt </li>` 
-     `<li>lodeczki 170 g.</li>`
-      `<li>rollsy 1.5 szt </li>`
+    div.innerHTML = 
+    `<li>Pieczywo 3 szt </li>`
+    `<li>krzydelka 5 szt</li>` 
+    `<li>lodeczki 170 g.</li>`
+    `<li>rollsy 1.5 szt </li>`
+
+}
+
+button2.addEventListener('touchstart', createBox)
+
+function createBox(){
+    document.body.append(div)
+    div.innerHTML = `<li>Pieczywo 3 szt </li>` `<li>skrzydelka 5 szt  odeczki 170 g </li> rollsy 1.5 szt`
+     
+}
+
+button2.addEventListener('click',  createBox)
+
+function createBox(){
+    document.body.append(div)
+    div.innerHTML = 
+    `<li>Pieczywo 3 szt </li>`
+    `<li>krzydelka 5 szt</li>` 
+    `<li>lodeczki 170 g.</li>`
+    `<li>rollsy 1.5 szt </li>`
+
+}
+
+button3.addEventListener('touchstart', createBox)
+
+function createBox(){
+    document.body.append(div)
+    div.innerHTML = `<li>Pieczywo 3 szt </li>` `<li>skrzydelka 5 szt  odeczki 170 g </li> rollsy 1.5 szt`
+     
+}
+
+button3.addEventListener('click',  createBox)
+
+function createBox(){
+    document.body.append(div)
+    div.innerHTML = 
+    `<li>Pieczywo 3 szt </li>`
+    `<li>krzydelka 5 szt</li>` 
+    `<li>lodeczki 170 g.</li>`
+    `<li>rollsy 1.5 szt </li>`
+
+}
+
+button4.addEventListener('touchstart', createBox)
+
+function createBox(){
+    document.body.append(div)
+    div.innerHTML = `<li>Pieczywo 3 szt </li>` `<li>skrzydelka 5 szt  odeczki 170 g </li> rollsy 1.5 szt`
+     
+}
+
+button4.addEventListener('click',  createBox)
+
+function createBox(){
+    document.body.append(div)
+    div.innerHTML = 
+    `<li>Pieczywo 3 szt </li>`
+    `<li>krzydelka 5 szt</li>` 
+    `<li>lodeczki 170 g.</li>`
+    `<li>rollsy 1.5 szt </li>`
 
 }
